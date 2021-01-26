f578bd2136091975448e4c8ac857ccbb370f253f
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-01-26 14:41:00 +0400

    Update login.php
    on est bien redirigé vers espace perso lors de la connexion
    on cherche les infos dans une copie de la table de la bdd

--- src/functions/login.php
+++ src/functions/login.php
@@ -11,7 +11,7 @@ function login()
         }
 
         // Préparation de la requête SQL : on va comparer le nom de la team et l'email du chef d'équipe
-        $req = $bdd->prepare('SELECT * FROM la_longue WHERE team = :team AND email_relayeur_1 = :email_relayeur_1');
+        $req = $bdd->prepare('SELECT * FROM la_longue_copie WHERE team = :team AND email_relayeur_1 = :email_relayeur_1');
 
         // On exécute la requête SQL en comparant les infos de la BDD avec les infos rentrées dans le formulaire par l'utilisateur
         $req->execute(array(
@@ -26,7 +26,7 @@ function login()
         } else { // Les identifiants correspondent avec ce qui se trouve dans la BDD
             $error_login = '';
             $_SESSION['user'] = $result; // On stocke temporairement les infos de l'équipe dans la variable $_SESSION
-            header('Location: index.php?page=choix_connexion'); // On redirige l'utilisateur vers son espace personnel
+            header('Location: index.php?page=espace_personnel'); // On redirige l'utilisateur vers son espace personnel
             die;
         }
     }
