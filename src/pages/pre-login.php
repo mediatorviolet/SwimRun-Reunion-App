6e0903b2fcacab8a7540ff05654ddeeaf2e4f213
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-01-26 11:26:49 +0400

    responsive pre-login

--- src/pages/pre-login.php
+++ src/pages/pre-login.php
@@ -1,15 +1,21 @@
 <div class="container-fluid main-page d-flex justify-content-center align-items-center">
-    <div class="card text-center shadow-lg" style="width: 25rem;">
+    <div class="card text-center shadow-lg border-0" style="width: 25rem;">
         <div class="card-header fw-bold">
             Je suis inscrit à :
         </div>
         <div class="card-body py-5">
-            <a href="index.php?page=connexion" class="btn me-4 rounded-0">
-                La Longue
-            </a>
-            <a href="#" class="btn ms-4 rounded-0">
-                La Courte
-            </a>
+            <div class="row">
+                <div class="col-sm">
+                    <a href="index.php?page=connexion" class="btn me-sm-2 mb-sm-0 mb-4 rounded-0">
+                        La Longue
+                    </a>
+                </div>
+                <div class="col-sm">
+                    <a href="#" class="btn ms-sm-2 mt-sm-0 mt-4 rounded-0">
+                        La Courte
+                    </a>
+                </div>
+            </div>
         </div>
         <div class="card-footer text-muted">
             Lorem ipsum dolor sit amet.
