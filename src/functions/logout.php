58f75af880c706422f26629558f9de869171716f
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-01-27 16:44:39 +0400

    changement header lors de la déconnexion

--- src/functions/logout.php
+++ src/functions/logout.php
@@ -5,7 +5,7 @@ function logout()
     if ($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_POST['deconnexion'])) { // Quand user clique sur btn deconnexion on détruit sa session et on reviens à l'accueil
         $_SESSION['user'] = false;
         session_destroy();
-        header('Location: index.php');
+        header('Location: index.php?page=connexion');
         die;
     }
 }
