a34977fb0cea93196b24fd9714c0b080a9d7654e
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-01-28 11:26:52 +0400

    nav dashboard admin

--- src/pages/admin.php
+++ src/pages/admin.php
@@ -1,1 +1,19 @@
-AAAAAAAAAAAAAAAAAAAAADDDDDDDDDDDDDDDDDMMMMMMMMMIIIIIIIIIIIIIIIIIINNNNNNNNNNNNNNNNN
+<?php include 'functions/logout.php' ?>
+<nav class="nav justify-content-between bg-light shadow fixed-top">
+    <div class="d-flex">
+        <p class="navbar-brand fw-bold ms-4 my-2">Tableau de bord administrateur</p>
+    </div>
+    <div class="d-flex">
+        <a href="" class="btn rounded-0 d-flex align-items-center">
+            En attente de validation <span class="badge ms-2">4</span>
+        </a>
+        <form action="<?= logout() ?>" method="post">
+            <button type="submit" name="deconnexion" class="btn rounded-0 ms-5 h-100">
+                Déconnexion
+            </button>
+        </form>
+    </div>
+</nav>
+<div class="container-fluid">
+
+</div>
