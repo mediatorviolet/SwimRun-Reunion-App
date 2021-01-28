2c661fdd8c6e7d018759b224a4da5ce14bfd74a9
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-01-28 11:57:24 +0400

    navbar admin dashboard

--- src/pages/admin.php
+++ src/pages/admin.php
@@ -1,17 +1,21 @@
 <?php include 'functions/logout.php' ?>
-<nav class="nav justify-content-between bg-light shadow fixed-top">
-    <div class="d-flex">
-        <p class="navbar-brand fw-bold ms-4 my-2">Tableau de bord administrateur</p>
-    </div>
-    <div class="d-flex">
-        <a href="" class="btn rounded-0 d-flex align-items-center">
-            En attente de validation <span class="badge ms-2">4</span>
-        </a>
-        <form action="<?= logout() ?>" method="post">
-            <button type="submit" name="deconnexion" class="btn rounded-0 ms-5 h-100">
-                Déconnexion
-            </button>
-        </form>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold" href="#">Tableau de bord administrateur</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <a href="" class="btn rounded-0 mt-lg-0 mt-4">
+                À valider <span class="badge ms-2">4</span>
+            </a>
+            <form action="<?= logout() ?>" method="post">
+                <button type="submit" name="deconnexion" class="btn rounded-0 mx-lg-4 mt-lg-0 mt-4 h-100">
+                    Déconnexion
+                </button>
+            </form>
+        </div>
     </div>
 </nav>
 <div class="container-fluid">
