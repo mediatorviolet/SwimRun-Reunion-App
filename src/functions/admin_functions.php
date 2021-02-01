829bf41413e39570a01cfdde140ed711eefac4e7
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-01 11:37:08 +0400

    création de la fonction principale admin

---
+++ src/functions/admin_functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function valider()
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' and (isset($_POST['valide']) or isset($_POST['non-valide']))) {
+        try { // Connexion à la BDD
+            $bdd = new PDO('mysql:host=127.0.0.1;dbname=swimrun-app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        } catch (Exception $e) { // Si erreur, on renvoi un message d'erreur
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        if (isset($_POST['valide'])) {
+            $bdd->exec('UPDATE en_attente SET etat = \'valide\' WHERE id_attente = "' . $_POST['id_attente'] . '"');
+        }
+
+        if (isset($_POST['non-valide'])) {
+            $bdd->exec('UPDATE en_attente SET etat = \'non_valide\' WHERE id_attente = "' . $_POST['id_attente'] . '"');
+        }
+
+        header('Location: index.php?page=admin');
+        // die;
+    }
+}
