5ecaca2af5c49e0cdb22a1f80a4c4f4b430569dc
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-01 20:08:24 +0400

    Create connexion_bdd.php

---
+++ src/functions/connexion_bdd.php
@@ -0,0 +1,12 @@
+<?php
+
+$bdd;
+function connexion_bdd()
+{
+    global $bdd;
+    try { // Connexion à la BDD
+        $bdd = new PDO('mysql:host=127.0.0.1;dbname=swimrun-app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch (Exception $e) { // Si erreur, on renvoi un message d'erreur
+        die('Erreur : ' . $e->getMessage());
+    }
+}
