ecb795dbdb5f4ff679e534f01be68e5b9791f02b
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-03 16:26:14 +0400

    modale s'ouvre avec un textarea pour saisir le motif du refus de la modif

--- src/functions/admin_functions.php
+++ src/functions/admin_functions.php
@@ -1,7 +1,9 @@
 <?php
 
+$bool;
 function valider()
 {
+    global $bool;
     if ($_SERVER['REQUEST_METHOD'] == 'POST' and (isset($_POST['valide']) or isset($_POST['non-valide']))) {
         try { // Connexion à la BDD
             $bdd = new PDO('mysql:host=127.0.0.1;dbname=swimrun-app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
@@ -11,17 +13,22 @@ function valider()
 
         if (isset($_POST['valide'])) {
             $bdd->exec('UPDATE en_attente SET etat = \'valide\' WHERE id_attente = "' . $_POST['id_attente'] . '"');
+            $bool = true;
         }
 
         if (isset($_POST['non-valide'])) {
-            $bdd->exec('UPDATE en_attente SET etat = \'non_valide\' WHERE id_attente = "' . $_POST['id_attente'] . '"');
+            if (!empty($_POST['motif'])) {
+                $bdd->exec('UPDATE en_attente SET etat = \'non_valide\' WHERE id_attente = "' . $_POST['id_attente'] . '"');
+                $bool = false;
+            }
         }
-        header('Location: index.php?page=admin');
-        die;
+        // header('Location: index.php?page=admin');
+        // die;
     }
 }
 
 // Fonction qui compare les valeurs de la table team avec celles de la table en_attente, si elles sont différentes, la case correspondante du tableau est colorée en jaune
-function highlight_change($t, $e) {
+function highlight_change($t, $e)
+{
     echo $t != $e ? "<td class='table-warning'>" . $e . "</td>" : "<td>" . $e . "</td>";
 }
--- src/pages/admin.php
+++ src/pages/admin.php
@@ -16,7 +16,7 @@ function badge_count($etat)
 }
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
     <div class="container-fluid">
         <a class="navbar-brand fw-bold mx-lg-4" href="#">Tableau de bord administrateur</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,8 +32,23 @@ function badge_count($etat)
     </div>
 </nav>
 
-<div class="container-fluid">
-    <ul class="nav nav-tabs nav-fill mt-5 pt-5 mx-lg-4" id="myTab" role="tablist">
+<div class="container-fluid my-5">
+    <?php
+    if (isset($_POST['valide']) or isset($_POST['non-valide'])) {
+        if ($bool) {
+            echo '<div class="alert alert-success alert-dismissible fade show col-6 mx-auto mb-5 text-center fw-bold shadow" role="alert">';
+            echo '<span>Modifications validées.</span>';
+            echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+            echo '</div>';
+        } else {
+            echo '<div class="alert alert-warning alert-dismissible fade show col-6 mx-auto mb-5 text-center fw-bold shadow" role="alert">';
+            echo '<span>Modifications non validées.</span>';
+            echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+            echo '</div>';
+        }
+    }
+    ?>
+    <ul class="nav nav-tabs nav-fill mx-lg-4" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
             <a class="nav-link fw-bold active" id="a-valider-tab" data-bs-toggle="tab" href="#a-valider" role="tab" aria-controls="a-valider" aria-selected="true">
                 À valider <span class="badge ms-2"><?= badge_count('a_valider') ?></span>
@@ -53,12 +68,7 @@ function badge_count($etat)
     <div class="tab-content" id="myTabContent">
         <div class="tab-pane fade show active mt-5" id="a-valider" role="tabpanel" aria-labelledby="a-valider-tab">
             <?php
-
             $req = $bdd->query('SELECT t.*, e.* FROM team t RIGHT JOIN en_attente e ON e.id_team = t.id WHERE e.etat = \'a_valider\'');
-            // $result = $req->fetchAll(PDO::FETCH_ASSOC);
-            // echo '<pre>';
-            // print_r($result);
-            // echo '</pre>';
 
             while ($donnees = $req->fetch()) { ?>
 
@@ -147,9 +157,33 @@ function badge_count($etat)
                         </tbody>
                     </table>
                     <form action="<?= valider() ?>" method="post">
-                        <input type="hidden" name="id_attente" value="<?= $donnees['e.id_attente'] ?>">
+                        <input type="hidden" name="id_attente" value="<?= $donnees['id_attente'] ?>">
                         <button type="submit" name="valide" class="btn rounded-0 me-4">Valider</button>
-                        <button type="submit" name="non-valide" class="btn rounded-0">Ne pas valider</button>
+
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn rounded-0" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Ne pas valider
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title fw-bold" id="exampleModalLabel">Motif de refus :</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body text-center">
+                                        <textarea name="motif" id="" cols="40" rows="10" placeholder="Entrez un motif de refus"></textarea>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn rounded-0" data-bs-dismiss="modal">Annuler</button>
+                                        <button type="submit" name="non-valide" class="btn rounded-0">Envoyer</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </form>
                 </div>
             <?php
@@ -160,18 +194,12 @@ function badge_count($etat)
         </div>
         <div class="tab-pane fade mt-5" id="non-valide" role="tabpanel" aria-labelledby="non-valide-tab">
             <?php
-            try { // Connexion à la BDD
-                $bdd = new PDO('mysql:host=127.0.0.1;dbname=swimrun-app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            } catch (Exception $e) { // Si erreur, on renvoi un message d'erreur
-                die('Erreur : ' . $e->getMessage());
-            }
-
             $req = $bdd->query('SELECT t.*, e.* FROM team t RIGHT JOIN en_attente e ON e.id_team = t.id WHERE e.etat = \'non_valide\'');
 
             while ($donnees = $req->fetch()) { ?>
 
                 <div class="table-responsive mb-5 mx-lg-4">
-                <table class="table table-hover table-bordered border-dark shadow">
+                    <table class="table table-hover table-bordered border-dark shadow">
                         <thead>
                             <tr>
                                 <th scope="col"><?= substr($donnees['rsfp_product'], 20) . ' | ' . $donnees['team'] . ' | N° ' . $donnees['id_team'] ?></th>
@@ -263,18 +291,12 @@ function badge_count($etat)
         </div>
         <div class="tab-pane fade mt-5" id="valide" role="tabpanel" aria-labelledby="valide-tab">
             <?php
-            try { // Connexion à la BDD
-                $bdd = new PDO('mysql:host=127.0.0.1;dbname=swimrun-app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            } catch (Exception $e) { // Si erreur, on renvoi un message d'erreur
-                die('Erreur : ' . $e->getMessage());
-            }
-
             $req = $bdd->query('SELECT t.*, e.* FROM team t RIGHT JOIN en_attente e ON e.id_team = t.id WHERE e.etat = \'valide\'');
 
             while ($donnees = $req->fetch()) { ?>
 
                 <div class="table-responsive mb-5 mx-lg-4">
-                <table class="table table-hover table-bordered border-dark shadow">
+                    <table class="table table-hover table-bordered border-dark shadow">
                         <thead>
                             <tr>
                                 <th scope="col"><?= substr($donnees['rsfp_product'], 20) . ' | ' . $donnees['team'] . ' | N° ' . $donnees['id_team'] ?></th>
