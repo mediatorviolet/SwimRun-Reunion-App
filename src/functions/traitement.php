d4280a615db9f76519bb8c3498502f7d86184d50
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-03 11:32:56 +0400

    envoi des modifs OK
    ajout s'il faut ajouter modif s'il faut modifier
    plus d'erreur lorsqu'on envoie pas de certificat médical
    Commentaire du code

--- src/functions/traitement.php
+++ src/functions/traitement.php
@@ -1,12 +1,14 @@
 <?php
 $count = 0;
 $required_input = [];
+// Fonction qui gère la validation du formulaire
 function validation_form()
 {
     global $count;
     global $required_input;
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['send_modif'])) {
         $count = 0;
+        // Liste des inputs requis pour que le formulaire s'envoie
         $required_input = array(
             'nom1',
             'prenom1',
@@ -26,18 +28,20 @@ function validation_form()
             'licence2'
         );
 
+        // Si le coureur est licencié FFTri on rajoute num_licence et club à la liste des inputs requis
         if ($_POST['licence1'] == 'FFTri') {
             array_push($required_input, 'num_licence1', 'club1');
         }
 
+        if ($_POST['licence2'] == 'FFTri') {
+            array_push($required_input, 'num_licence2', 'club2');
+        }
+
+        // Si le coureur est NON-LICENCIE et que l'input file n'est pas vide, on appelle la fonction upload
         if ($_POST['licence1'] == 'NON-LICENCIE') {
             if (!empty($_FILES['certif1'])) {
                 upload("certif1");
             }
-        }
-
-        if ($_POST['licence2'] == 'FFTri') {
-            array_push($required_input, 'num_licence2', 'club2');
         }
 
         if ($_POST['licence2'] == 'NON-LICENCIE') {
@@ -46,29 +50,37 @@ function validation_form()
             }
         }
 
+        // Pour chaque input dans le tableau $required_input
         foreach ($required_input as $input) {
-            if (empty($_POST["$input"])) {
+            if (empty($_POST["$input"])) { // Si un input est vide, le compteur augmente
                 $count++;
-            } else {
+            } else { // Sinon si l'input n'est pas vide, on lui passe les methodes trim() et htmlentities() -> faille XSS
                 $_POST["$input"] = trim(htmlentities($_POST["$input"], ENT_QUOTES));
             }
         }
 
+        // Si le compteur est <= 0, alors on appelle la fonction envoi_form()
         if ($count <= 0) {
             envoi_form();
         }
     }
 }
 
+// Fonction qui gère l'envoi des infos dans la BDD
 function envoi_form()
 {
     try { // Connexion à la BDD
-        $bdd = new PDO('mysql:host=localhost;dbname=swimrun-app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $bdd = new PDO('mysql:host=127.0.0.1;dbname=swimrun-app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     } catch (Exception $e) { // Si erreur, on renvoi un message d'erreur
         die('Erreur : ' . $e->getMessage());
     }
 
-    $req = $bdd->prepare('INSERT INTO en_attente(id_team, nom_relayeur_1, prenom_relayeur_1, sexe1, tshirt1, annee_naissance_1, telephone1, licence_1, numero_licence_1, club1, certificat1, nom_relayeur_2, prenom_relayeur_2, sexe2, tshirt2, annee_naissance_2, telephone2, licence_2, numero_licence_2, club2, certificat2, etat) VALUES(:id_team, :nom_relayeur_1, :prenom_relayeur_1, :sexe1, :tshirt1, :annee_naissance_1, :telephone1, :licence_1, :numero_licence_1, :club1, :certificat1, :nom_relayeur_2, :prenom_relayeur_2, :sexe2, :tshirt2, :annee_naissance_2, :telephone2, :licence_2, :numero_licence_2, :club2, :certificat2, :etat)');
+    // On fait appel à la fonction verif_team() pour savoir si on doit ajouter une ligne ou modifier une ligne déjà existante
+    if (verif_team() == true) {
+        $req = $bdd->prepare('UPDATE en_attente SET id_team = :id_team, nom_relayeur_1 = :nom_relayeur_1, prenom_relayeur_1 = :prenom_relayeur_1, sexe1 = :sexe1, tshirt1 = :tshirt1, annee_naissance_1 = :annee_naissance_1, telephone1 = :telephone1, licence_1 = :licence_1, numero_licence_1 = :numero_licence_1, club1 = :club1, certificat1 = :certificat1, nom_relayeur_2 = :nom_relayeur_2, prenom_relayeur_2 = :prenom_relayeur_2, sexe2 = :sexe2, tshirt2 = :tshirt2, annee_naissance_2 = :annee_naissance_2, telephone2 = :telephone2, licence_2 = :licence_2, numero_licence_2 = :numero_licence_2, club2 = :club2, certificat2 = :certificat2, etat = :etat WHERE id_team = "' . $_SESSION['user']['id'] . '"');
+    } else {
+        $req = $bdd->prepare('INSERT INTO en_attente(id_team, nom_relayeur_1, prenom_relayeur_1, sexe1, tshirt1, annee_naissance_1, telephone1, licence_1, numero_licence_1, club1, certificat1, nom_relayeur_2, prenom_relayeur_2, sexe2, tshirt2, annee_naissance_2, telephone2, licence_2, numero_licence_2, club2, certificat2, etat) VALUES(:id_team, :nom_relayeur_1, :prenom_relayeur_1, :sexe1, :tshirt1, :annee_naissance_1, :telephone1, :licence_1, :numero_licence_1, :club1, :certificat1, :nom_relayeur_2, :prenom_relayeur_2, :sexe2, :tshirt2, :annee_naissance_2, :telephone2, :licence_2, :numero_licence_2, :club2, :certificat2, :etat)');
+    }
 
     $req->execute(array(
         'id_team' => $_SESSION['user']['id'],
@@ -81,7 +93,7 @@ function envoi_form()
         'licence_1' => $_POST['licence1'],
         'numero_licence_1' => $_POST['num_licence1'],
         'club1' => $_POST['club1'],
-        'certificat1' => 'uploads/'. md5(pathinfo($_FILES["certif1"]["name"])['filename']) . '.' . pathinfo($_FILES["certif1"]["name"])['extension'],
+        'certificat1' => isset(pathinfo($_FILES["certif1"]["name"])['extension']) ? 'uploads/' . md5(pathinfo($_FILES["certif1"]["name"])['filename']) . '.' . pathinfo($_FILES["certif1"]["name"])['extension']  : '',
         'nom_relayeur_2' => $_POST['nom2'],
         'prenom_relayeur_2' => $_POST['prenom2'],
         'sexe2' => $_POST['sexe2'],
@@ -91,14 +103,12 @@ function envoi_form()
         'licence_2' => $_POST['licence2'],
         'numero_licence_2' => $_POST['num_licence2'],
         'club2' => $_POST['club2'],
-        'certificat2' => 'uploads/'. md5(pathinfo($_FILES["certif2"]["name"])['filename']) . '.' . pathinfo($_FILES["certif2"]["name"])['extension'],
+        'certificat2' => isset(pathinfo($_FILES["certif2"]["name"])['extension']) ? 'uploads/' . md5(pathinfo($_FILES["certif2"]["name"])['filename']) . '.' . pathinfo($_FILES["certif2"]["name"])['extension'] : '',
         'etat' => 'a_valider'
     ));
-    // header('Location: index.php?page=espace_personnel');
-    // die;
 }
 
-
+// Fonction qui gère l'upload des fichiers .pdf
 function upload($certif)
 {
     global $count;
@@ -108,7 +118,7 @@ function upload($certif)
             $extensionUpload = $infosFichier["extension"];
             $nomFichier = md5($infosFichier['filename']) . '.' . $infosFichier['extension'];
             if ($extensionUpload == "pdf") {
-                move_uploaded_file($_FILES["$certif"]["tmp_name"], 'uploads/'. $nomFichier);
+                move_uploaded_file($_FILES["$certif"]["tmp_name"], 'uploads/' . $nomFichier);
             } else {
                 $count++;
             }
@@ -117,5 +127,27 @@ function upload($certif)
         }
     } else {
         $count++;
+    }
+}
+
+// Fonction qui vérifie si l'équipe à déjà effectué une modif de ses infos
+function verif_team()
+{
+    try { // Connexion à la BDD
+        $bdd = new PDO('mysql:host=127.0.0.1;dbname=swimrun-app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch (Exception $e) { // Si erreur, on renvoi un message d'erreur
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    // On cherche si $_SESSION['user']['id'] correspond à un id_team dans la table en_attente
+    $req = $bdd->query('SELECT EXISTS ( SELECT id_team FROM en_attente WHERE id_team = ' . $_SESSION['user']['id'] . ') AS team_exists');
+
+    $duplicate = $req->fetch();
+    if ($duplicate['team_exists']) {
+        // Si on trouve une correspondance on renvoie true
+        return true;
+    } else {
+        // Sinon on renvoie false
+        return false;
     }
 }
