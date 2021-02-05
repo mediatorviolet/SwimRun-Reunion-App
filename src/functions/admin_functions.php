19391b5afe5a712b3d88a9c8c736faa4b8c0f23b
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-05 16:36:45 +0400

    Si les modifs ne sont pas valdiées par admin, l'équipe reçoit un mail

--- src/functions/admin_functions.php
+++ src/functions/admin_functions.php
@@ -1,5 +1,54 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'C:\Users\Antoine\Documents\GitHub\SwimRun-Reunion-App\vendor\autoload.php';
+
+function send_mail($to, $subject, $template, $p1, $p2, $motif, $team, $code)
+{
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+    // On force l'encodage en UTF-8 pour ne pas avoir de problèmes d'affichage
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
+
+
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host       = 'mail.gmx.com ';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'emaileur@gmx.fr';
+        $mail->Password   = 'Pouetpouet123';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        //Recipients
+        $mail->setFrom('emaileur@gmx.fr', 'Swimrun Réunion');
+        $mail->addAddress($to);
+
+        // Content
+        $message = file_get_contents("./templates/$template");
+        $message = str_replace('%prenom1%', $p1, $message);
+        $message = str_replace('%prenom2%', $p2, $message);
+        $message = str_replace('%motif%', $motif, $message);
+        $message = str_replace('%team%', $team, $message);
+        $message = str_replace('%code_invite%', $code, $message);
+
+        $mail->Subject = $subject;
+        $mail->MsgHTML($message);
+        $mail->AltBody = strip_tags($message);
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
 $bool;
 function valider()
 {
@@ -18,12 +67,21 @@ function valider()
 
         if (isset($_POST['non-valide'])) {
             if (!empty($_POST['motif'])) {
+                $req = $bdd->query('SELECT e.id_attente, e.id_team, e.email1 email1, e.prenom1 p1, e.prenom2 p2, t.id, t.team team, t.code_invite code FROM team t RIGHT JOIN en_attente e ON e.id_team = t.id WHERE e.id_attente = "' . $_POST['id_attente'] . '"');
+                $donnees = $req->fetch();
+                $to = $donnees['email1'];
+                $p1 = html_entity_decode($donnees['p1']);
+                $p2 = html_entity_decode($donnees['p2']);
+                $team = html_entity_decode($donnees['team']);
+                $code = $donnees['code'];
+                $motif = $_POST['motif'];
+                $template = 'non-valide-mail.html';
+                // send_mail($to, 'Modifications non validées', $template, $p1, $p2, $motif, $team, $code);
+                $req->closeCursor();
                 $bdd->exec('UPDATE en_attente SET etat = \'non_valide\' WHERE id_attente = "' . $_POST['id_attente'] . '"');
                 $bool = false;
             }
         }
-        // header('Location: index.php?page=admin');
-        // die;
     }
 }
 
