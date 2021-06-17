101e302696a2ef5021bf4bc25915dca4b3383ae8
Antoine SCHILT <antoineschilt@gmail.com>
2021-06-17 20:55:21 +0400

    with env variables

--- src/pages/adminDashboard.php
+++ src/pages/adminDashboard.php
@@ -70,7 +70,7 @@ include 'src/functions/admin_functions.php';
         <tbody>
             <?php
             try { // Connexion à la BDD
-                $bdd = new PDO('mysql:host=127.0.0.1;dbname=swimrun-app;charset=utf8', 'antoine', 'password', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                $bdd = new PDO('mysql:host=' . $_ENV["DB_HOST"] . ';dbname=' . $_ENV["DB_NAME"] . ';charset=utf8', $_ENV["MYSQL_USERNAME"], $_ENV["MYSQL_PASSWORD"], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             } catch (Exception $e) { // Si erreur, on renvoi un message d'erreur
                 die('Erreur : ' . $e->getMessage());
             }
